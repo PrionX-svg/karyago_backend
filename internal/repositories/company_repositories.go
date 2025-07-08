aef8a70cb00fcbd9d3ccbcb7f693409e236df637
sshivahh <russelshivah16@gmail.com>
2025-07-08 17:27:31 +0700

    [BE] Company API & Docs
    -: Complete CRUD for company

--- internal/repositories/company_repositories.go
+++ internal/repositories/company_repositories.go
@@ -8,7 +8,10 @@ import (
 
 type CompanyRepositories interface {
 	Create(company *models.Company) error
-
+	GetAll() ([]models.Company, error)
+	GetByUUID(uuid string) (models.Company, error)
+	Update(updatedCompany *models.Company) error
+	Delete(targetCompany *models.Company) error 
 }
 
 type companyRepositories struct {
@@ -20,7 +23,37 @@ func NewCompanyRepository(db *gorm.DB) CompanyRepositories {
 }
 
 func (r *companyRepositories) Create(company *models.Company) error {
-	if err := r.db.Create(company).Error; err != nil{
+	if err := r.db.Create(company).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *companyRepositories) GetAll() ([]models.Company, error) {
+	var companies []models.Company
+	if err := r.db.Find(&companies).Error; err != nil {
+		return nil, err
+	}
+	return companies, nil
+}
+
+func (r *companyRepositories) GetByUUID(uuid string) (models.Company, error) {
+	var company models.Company
+	if err := r.db.Where("uuid = ?", uuid).First(&company).Error; err != nil {
+		return models.Company{}, err
+	}
+	return company, nil
+}
+
+func (r *companyRepositories) Update(updatedCompany *models.Company) error {
+	if err := r.db.Save(updatedCompany).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *companyRepositories) Delete(targetCompany *models.Company) error {
+	if err := r.db.Delete(targetCompany).Error; err != nil {
 		return err
 	}
 	return nil
