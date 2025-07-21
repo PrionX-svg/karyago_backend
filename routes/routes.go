a2c387b365713f66e7ae56bdc4382a7688fc8358
vincentius.marco <vincentius.marco@createit.co.id>
2025-07-21 14:30:16 +0700

    fix upload

--- routes/routes.go
+++ routes/routes.go
@@ -40,8 +40,8 @@ func SetupRoutes(app *fiber.App) {
 
 	r2Client, err := r2.NewR2Client(
 		os.Getenv("R2_ENDPOINT"),
-		os.Getenv("R2_ACCESS_KEY"),
-		os.Getenv("R2_SECRET_KEY"),
+		os.Getenv("R2_ACCESS_KEY_ID"),
+		os.Getenv("R2_SECRET_ACCESS_KEY"),
 		true,
 	)
 	if err != nil {
